92e6b2ceaf6a057acb2c9da9b0b1ae54f0ac407d
Alex <jalexgould@gmail.com>
2020-09-12 13:45:59 -0400

    first push

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// All Local Prices
+
+// Landing Page which shows a local search bar that will search for local
+// restaurants, cafes, coffee. 
+
+// This drop down bar will then redirect to a section further down the landing page once clicked. 
+
+// The secondary section on the landing page will display 3-4 options for the selected filter chosen
+// on the landing page. 
+
+// At the bottom of secondary section on the landing page will be a button that says start new
+// search. 
+
+// This button once clicked will redirect back to landing page for a new search. 
