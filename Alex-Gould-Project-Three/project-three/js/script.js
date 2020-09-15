6091d5f37a5e6984e6d3825e51aea5c0149cf9e1
Alex <jalexgould@gmail.com>
2020-09-15 04:28:55 -0400

    final 4:30am

--- Alex-Gould-Project-Three/project-three/js/script.js
+++ Alex-Gould-Project-Three/project-three/js/script.js
@@ -1,19 +1,4 @@
-// All Local Prices
 
-// Landing Page which shows a local search bar that will search for local
-// restaurants, cafes, coffee. 
-
-// This drop down bar will then redirect to a section further down the landing page once clicked. 
-
-// The secondary section on the landing page will display 3-4 options for the selected filter chosen
-// on the landing page. 
-
-// At the bottom of secondary section on the landing page will be a button that says start new
-// search. 
-
-// This button once clicked will redirect back to landing page for a new search. 
-
-//App object
 const localApp = {};
 const venues = {
 
@@ -41,18 +26,14 @@ const venues = {
 
 // Initializing App 
 
-localApp.init = function(){
+localApp.init = function(business, venue){
    $('#business').on('change', function(){
       const business = $(this).val();
       
       const businessType = $(this).find(':selected').text();
-      const venue = $(this).find.find(':selected').text();
-      // console.log(businessType);
 
-      // const businessName =$(this).find(':selected').text;
-      // listen for the change on the drop down menu and grab the value
       $('.title-Card').html(`<h2>${businessType} </h2>`)
-      })
+   })
    
 }
 // Document ready function
